aa88aebb52fec3ee80d6451873eb9c10ca88654b
Furkan Işıtan <furkanisitan96@gmail.com>
2021-08-24 17:46:39 +0300

    Add csharp101 - if-else-ternany

---
+++ csharp-101/src/if-else-ternany/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace if_else_ternany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("Good afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("Good night.");
+            }
+
+            string res = time <= 18 ? "Good morning." : "Good night.";
+            res = time >= 6 && time < 11 ? "Good morning." : time <= 18 ? "Good afternoon." : "Good night.";
+            Console.WriteLine(res);
+        }
+    }
+}
