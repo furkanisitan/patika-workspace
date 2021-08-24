356c7489ab407004561e558d85431c0133224b06
Furkan Işıtan <furkanisitan96@gmail.com>
2021-08-24 17:53:51 +0300

    Add csharp101 - hackerrank - conditional-statements-intro

---
+++ csharp-101/src/hackerrank/conditional-statements-intro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace conditional_statements_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            string weird = "Weird", notweird = "Not Weird";
+
+            if (n % 2 == 1)
+                Console.WriteLine(weird);
+            else
+                if ((n >= 2 && n <= 5) || n > 20)
+                Console.WriteLine(notweird);
+            else if (n >= 6)
+                Console.WriteLine(weird);
+        }
+    }
+}
