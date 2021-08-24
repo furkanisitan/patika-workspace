0e856faeedf40f9b12fa1dfe449f35c2da371183
Furkan Işıtan <furkanisitan96@gmail.com>
2021-08-24 15:51:37 +0300

    Add csharp101 - console-programming

---
+++ csharp-101/src/console-programming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.Write("Enter firstname: ");
+            string firstname = Console.ReadLine();
+
+            Console.Write("Enter lastname: ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine($"Hello {firstname} {lastname}.");
+        }
+    }
+}
