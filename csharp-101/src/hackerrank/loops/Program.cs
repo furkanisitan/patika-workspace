0024b70dee771002f1a8fb29c4d5f2a2df48f801
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-04 13:40:40 +0300

    Add csharp101 - hackerrank - loops

---
+++ csharp-101/src/hackerrank/loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine().Trim());
+            printMultiples(number, 10);
+        }
+
+        static void printMultiples(int number, int limit)
+        {
+            for (int i = 1; i <= limit; i++)
+                Console.WriteLine($"{number} x {i} = {number * i}");
+
+        }
+
+    }
+}
